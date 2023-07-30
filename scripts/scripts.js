1c0ed58ba8d6e0a5896ae9377bc2f1253e99bdc9
Tortugon09 <axelramos001@gmail.com>
2023-07-30 17:51:43 -0600

    terminado

---
+++ scripts/scripts.js
@@ -0,0 +1,49 @@
+
+document.addEventListener('DOMContentLoaded', function() {
+    var labels = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo'];
+    var data = [10, 5, 8, 12, 7];
+
+    var ctx = document.getElementById('lineChart').getContext('2d');
+    var lineChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: labels,
+            datasets: [{
+                label: 'Ventas',
+                data: data,
+                borderColor: '#B2D6E7',
+                backgroundColor: 'rgba(75, 192, 192, 0.2)',
+                borderWidth: 2,
+            }]
+        },
+        options: {
+            responsive: true,
+            maintainAspectRatio: false,
+            scales: {
+                x: {
+                    grid: {
+                        display: false
+                    }
+                },
+                y: {
+                    grid: {
+                        display: false
+                    }
+                }
+            },
+            elements: {
+                line: {
+                    tension: 0,
+                },
+                point: {
+                    radius: 0,
+                }
+            },
+            plugins: {
+                legend: {
+                    display: false,
+                }
+            }
+        }
+    });
+});
